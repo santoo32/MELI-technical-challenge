8f72a4ed6506b4cdc5c257e54073f07cd7c59b7f
santiago <santiago@serfe.com>
2021-04-24 11:33:33 -0300

    dev - error handling

--- api/models/Response.js
+++ api/models/Response.js
@@ -1,6 +1,5 @@
 class Response {
   constructor() {
-    this.hasError = false;
     this.payload = {
       author: {
         'name': "Santiago",
@@ -14,17 +13,7 @@ class Response {
   }
 
   setError(err) {
-    this.hasError = true;
-    if (err.message) {
-      this.payload.error.message = err.message;
-    } else if (Array.isArray(err) && err[0].msg) {
-      this.payload.error = {};
-      err.map(item => {
-        this.payload.error[item.param] = item.msg;
-      })
-    } else {
-      this.payload.error.message = err;
-    }
+    this.payload.err = err
   }
 
   addErrorData(err) {
