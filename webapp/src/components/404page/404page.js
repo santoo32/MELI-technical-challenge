5ca8553338f6aecc790873c9d95d8729c4e7c510
santoo32 <santiagot.roa@gmail.com>
2021-04-29 22:43:27 -0300

    404 page

---
+++ webapp/src/components/404page/404page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./404page.scss";
+
+function NotFoundPage() {
+  return (
+    <div id="main">
+      <div class="fof">
+        <h1>Parece que esta página no existe</h1>
+      </div>
+    </div>
+  );
+}
+
+export default NotFoundPage;
