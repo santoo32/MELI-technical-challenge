2c6b7aae4246c94041ec05ec3644b929c8c7e9e3
santoo32 <santiagot.roa@gmail.com>
2021-05-01 11:06:06 -0300

    Initial version of component separation and search

--- webapp/src/routes.js
+++ webapp/src/routes.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import SearchView from './views/searchView/searchView'
-import ProductDetails from './views/productDetails/productDetails';
-import NotFoundPage from './views/404page/404page';
+import ProductDetails from "./views/productDetails/productDetails";
+import NotFoundPage from "./views/404page/404page";
+import SearchResults from "./views/searchResults/searchResults";
 
-const Routes = () => 
-
-    <Switch>
-      <Route exact path='/' component={SearchView} />
-      <Route path='/items/:id' component={ProductDetails} />
-      <Route component={NotFoundPage} />
-    </Switch>
-
+const Routes = ({ itemsList }) => (
+  <Switch>
+    <Route
+      exact
+      path="/items" /*component={SearchResults}*/
+      render={(props) => <SearchResults {...props} itemsList={itemsList} />}
+    />
+    <Route path="/items/:id" component={ProductDetails} />
+    <Route component={NotFoundPage} />
+  </Switch>
+);
 
 export default Routes;
--- webapp/src/views/searchView/searchView.js
+++ webapp/src/views/searchView/searchView.js
@@ -3,19 +3,25 @@ import SearchResults from "../searchResults/searchResults";
 import { getItems } from "../../services/itemService";
 import NavBar from "../../components/navBar/navBar";
 import './searchView.scss'
+import { useHistory } from "react-router";
 
-const SearchView = (props) => {
+
+const SearchView = ({itemsListUpdater}) => {
   const [input, setInput] = useState("");
-  const [itemsList, setItemsList] = useState([]);
+  const history = useHistory();
 
   const updateInput = async (input) => {
     setInput(input);
   };
 
   const search = () => {
-    getItems(input).then(({data}) => {
-        setItemsList(data.item);
-      });
+    //getItems(input).then(({data}) => {
+      itemsListUpdater(data.item);
+      history.push({
+        pathname: '/items',
+        search: `?search=${input}`
+      })
+    //});
   }
 
   return (
@@ -25,7 +31,6 @@ const SearchView = (props) => {
         setKeyword={updateInput}
         search={search}>
       </NavBar>
-      <SearchResults itemsList={itemsList} />
     </>
   );
 };
