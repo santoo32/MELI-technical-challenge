32b51b25cadd3eef70cfdae57b874eba31f17ca5
santoo32 <santiagot.roa@gmail.com>
2021-05-01 12:19:07 -0300

    Search results search on query param

--- webapp/src/views/searchResults/searchResults.js
+++ webapp/src/views/searchResults/searchResults.js
@@ -1,21 +1,33 @@
-import React from "react";
-import './searchResults.scss'
+import React, { useEffect, useState } from "react";
+import "./searchResults.scss";
+import queryString from 'query-string'
+import { useLocation } from 'react-router-dom'
+import { getItems } from "../../services/itemService";
+
+
 //The main role of this component is just to render all of the items
 // that are passed into it via props.
 const SearchResults = ({ itemsList = [] }) => {
-  
+  const [results, setResults] = useState([]);
+  const { search } = useLocation();
+  const values = queryString.parse(search);
+
+  useEffect(() => {
+    getItems(values.search).then(({ data }) => {
+      setResults(data.item);
+    });
+  }, [values.search]);
+
   return (
     <>
-      {itemsList.map((data) => {
-          return (
-            <div className="search-results-container" key={data.id}>
-              <img 
-              className="search-results-image"
-              src={data.picture}></img>
-              <h1 className="search-results-title">{data.price.amount}</h1>
-              <h1 className="search-results-desc">{data.title}</h1>
-            </div>
-          );
+      {results.map((data) => {
+        return (
+          <div className="search-results-container" key={data.id}>
+            <img className="search-results-image" src={data.picture}></img>
+            <h1 className="search-results-title">{data.price.amount}</h1>
+            <h1 className="search-results-desc">{data.title}</h1>
+          </div>
+        );
       })}
     </>
   );
