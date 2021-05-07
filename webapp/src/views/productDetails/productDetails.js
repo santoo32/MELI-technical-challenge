28a3dfb58865b92b3e659b9db87829b49c77f4a6
santoo32 <santiagot.roa@gmail.com>
2021-05-06 23:01:02 -0300

    Separation of concerns

--- webapp/src/views/productDetails/productDetails.js
+++ webapp/src/views/productDetails/productDetails.js
@@ -1,80 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { getItem } from "../../services/itemService";
-import { formatCurrency } from "../../components/common/priceFormatter";
 import "./productDetails.scss";
 import "../searchResults/searchResults.scss";
 import Breadcrumbs from "../../components/breadcrumbs/breadcrumbs";
+import ProductDisplay from "../../components/productDisplay/productDisplay";
+import WithLoading from "../../components/loaderScreen/spinner"
+const ProductDisplayWithLoading = WithLoading(ProductDisplay);
 
-class ProductDetails extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      product: null,
-    };
-  }
+const ProductDetails = ({match, history}) => {
+  const [product, setProduct] = useState(null);
+  const [hasFinished, sethasFinished] = useState(false);
 
-  componentDidMount() {
-    const { match, history } = this.props;
-
-    if (!match.params.id) {
-      history.push("/");
-    } else {
-      getItem(match.params.id)
+  useEffect(() => {
+    sethasFinished(false);
+    getItem(match.params.id)
         .then(({ data }) => {
+          sethasFinished(true);
           if (data.err) {
             //Product is not found
             history.push("/");
           } else {
-            this.setState({ product: data });
+            setProduct(data)
           }
         })
         .catch((err) => {
           //General error catcher
           console.log(err);
         });
-    }
-  }
+  }, [match.params.id]);
 
-  render() {
-    const { product } = this.state;
+
 
     return (
-      product && (
-      <section>
-        <Breadcrumbs categories={product.category}></Breadcrumbs>
-        <div className="product-detail-page">
-          <content>
-            <div className="product-detail-top">
-              {/* <div className="image-container"> */}
-              <img className="image-container" alt={product.title} src={product.picture}></img>
-              {/* </div> */}
-              <div className="product-detail-actions">
-                <div className="product-condition">
-                  {product.condition} - {product.sold_quantity} vendidos
-                </div>
-                <h1 className="product-title">{product.title}</h1>
-                <span className="item-price">
-                  {formatCurrency(
-                    product.price.amount,
-                    product.price.currency,
-                    "es-AR"
-                  )}
-                </span>
-                <button className="buy-button" aria-label="Comprar">Comprar</button>
-              </div>
-            </div>
-            <div className="product-detail-bottom">
-              <p>Descripcion del producto</p>
-              <div className="product-description">
-                <p>{product.description}</p>
-              </div>
-            </div>
-          </content>
-        </div>
-      </section>
-      )
+        <section>
+          <ProductDisplayWithLoading
+            results={product}
+            hasFinished={hasFinished}>
+          </ProductDisplayWithLoading>
+        </section>
     );
-  }
 }
 
 export default ProductDetails;
